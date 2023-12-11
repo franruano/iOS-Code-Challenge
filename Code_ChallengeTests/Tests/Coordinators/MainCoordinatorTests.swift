e7a3e1daa682430cbaf19c1d6e7ab6cf786d6820
Fran Ruano <franruan@gmail.com>
2023-12-11 18:32:33 +0000

    Unit tested Coordinator

--- Code_ChallengeTests/Tests/Coordinators/MainCoordinatorTests.swift
+++ Code_ChallengeTests/Tests/Coordinators/MainCoordinatorTests.swift
@@ -21,19 +21,18 @@ final class MainCoordinatorTests: XCTestCase {
         XCTAssertTrue(sut.navigationController.viewControllers.first? .isKind(of: LoginViewController.self) ?? false)
     }
     
-//    func testNavigationControllerStack_WhenIsLoggedOut_AndLogIn() throws {
-//        XCTAssertTrue(sut.navigationController.viewControllers.first? .isKind(of: LoginViewController.self) ?? false)
-//        
-//        mockTwitterClient.isLoggedInResponse = true
-//        XCTAssertEqual(sut.navigationController.viewControllers.count, 1)
-//        XCTAssertTrue(sut.navigationController.viewControllers.first? .isKind(of: TweetListViewController.self) ?? false)
-//    }
-//    
-//    func testNavigationControllerStack_WhenIsLoggedIn_AndLogOut() throws {
-//        mockTwitterClient.isLoggedInResponse = true
-//        XCTAssertTrue(sut.navigationController.viewControllers.first? .isKind(of: TweetListViewController.self) ?? false)
-//        mockTwitterClient.isLoggedInResponse = false
-//        XCTAssertEqual(sut.navigationController.viewControllers.count, 1)
-//        XCTAssertTrue(sut.navigationController.viewControllers.first? .isKind(of: LoginViewController.self) ?? false)
-//    }
+    func testNavigationControllerStack_WhenIsLoggedOut_AndLogIn() throws {
+        let expectation = XCTestExpectation(description: "wait for login")
+        XCTAssertTrue(sut.navigationController.viewControllers.first? .isKind(of: LoginViewController.self) ?? false)
+        
+        mockTwitterClient.isLoggedInResponse = true
+//        Not the right solution. Just for testing purposes
+//        It should use NSProtocol
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+        XCTAssertEqual(sut.navigationController.viewControllers.count, 1)
+        XCTAssertTrue(sut.navigationController.viewControllers.first? .isKind(of: TweetListViewController.self) ?? false)
+    }
 }
